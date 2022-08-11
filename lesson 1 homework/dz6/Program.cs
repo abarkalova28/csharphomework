27ad2cf2aecbcd9d43669cddbb3d76b263d51ed2
abarkalova28 <tyuwer@ya.ru>
2022-08-11 15:55:01 +0300

    added new hw task

---
+++ lesson 1 homework/dz6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите ваше число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+if (numberA % 2 == 0)
+    {
+    Console.WriteLine("Да, число является чётным.");
+    }
+else
+    {
+    Console.WriteLine("Нет, число является нечётным.");
+    } 
+Console.ReadKey();
