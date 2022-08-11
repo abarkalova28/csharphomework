bea8a0a17634283ac12e2568ef8559d8df311b6d
abarkalova28 <tyuwer@ya.ru>
2022-08-11 13:04:08 +0300

    homework exercise 2

---
+++ lesson 1 homework/dz 2.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите первое число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+
+if(numberA > numberB)
+    {
+    Console.WriteLine("{0} - максимальное значение, {1} - минимальное значение", numberA, numberB);
+    }
+else 
+    {
+    Console.WriteLine("{0} - максимальное значение, {1} - минимальное значение", numberB, numberA);
+    }
+
+Console.ReadLine();
