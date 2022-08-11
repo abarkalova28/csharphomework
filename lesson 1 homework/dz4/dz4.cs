704bd7a91c543acd97e916707225ab293d7abe24
abarkalova28 <tyuwer@ya.ru>
2022-08-11 15:47:07 +0300

    new hw task added

---
+++ lesson 1 homework/dz4/dz4.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите первое число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int numberС = Convert.ToInt32(Console.ReadLine());
+
+if(numberA > numberB && numberA > numberС)
+    {
+    Console.WriteLine("Максимальное число= " + numberA);
+    }
+
+else if(numberB > numberС)
+    {
+    Console.WriteLine("Максимальное число = " + numberB);
+    }
+else
+    {
+    Console.WriteLine("Максимальное число = " + numberС);
+    }
+Console.ReadLine();
