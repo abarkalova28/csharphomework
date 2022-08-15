f8663df7892c673895d8609cc71342d0fa3a8d6a
abarkalova28 <tyuwer@ya.ru>
2022-08-15 13:37:15 +0300

    new homework added

---
+++ lesson 2 homework/dz 13/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число: ");
+int Number = Convert.ToInt32(Console.ReadLine());
+string NumberCheck = Convert.ToString(Number);
+
+if(NumberCheck.Length > 2)
+    {
+    Console.WriteLine("Третья цифра числа: "+ NumberCheck[2]);
+    }
+else 
+    {
+    Console.WriteLine("Третьей цифры нет");
+    }
---
+++ lesson 2 homework/dz 15/Program.cs
@@ -0,0 +1,14 @@
+﻿using static System.Console;
+Console.WriteLine("Введите цифру, обозначающую день недели");
+Console.WriteLine("(Например: 1 - Понедельник, 2 - Вторник, 3 - Среда, 4 - Четверг, 5 - Пятница, 6 - Суббота, 7 - Воскресенье)");
+
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+if(dayNumber == 6 ^ dayNumber == 7)
+    {
+  Console.WriteLine("Да, это выходной день.");
+    }
+else 
+    {
+    Console.WriteLine("Нет, это не выходной день.");
+    }
