0cb3ab6dbea7ba49e32af8dbd12a13c550227b0f
abarkalova28 <tyuwer@ya.ru>
2022-08-15 13:08:11 +0300

    lesson 2 homework added

---
+++ lesson 2 homework/dz 10/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Write("Введите трёхзначное число: ");
+int ThreeDigit = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(ThreeDigit);
+Console.WriteLine("Вторая цифра трёхзначного числа: "+ stringNumber[1]);
