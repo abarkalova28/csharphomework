03a4ffb0ab6ec7e5b3af690c2139561b3db4c41d
abarkalova28 <tyuwer@ya.ru>
2022-08-17 19:34:36 +0300

    homework task 19 added

---
+++ lesson 3 homework/dz 19/Program.cs
@@ -0,0 +1,19 @@
+﻿int num, r, t, sum=0;
+
+Console.Write("Введите пятизначное число: ");
+num = Convert.ToInt32(Console.ReadLine()); 	
+string NumberCheck = Convert.ToString(num);
+
+if(NumberCheck.Length == 5) 
+    {
+        for(t=num; num!=0; num=num/10){
+         r=num % 10;
+         sum=sum*10+r;
+    }
+    if(t==sum)
+         Console.Write("{0} - число является палиндромом",t);
+    else
+         Console.Write("{0} - число не является палиндромом",t);
+         }
+else
+    Console.Write("Ошибка, число не является пятизначным, попробуйте ещё раз.");
