0392d1837e4cece44b7f6a9cbc54a62f51b50ae0
abarkalova28 <tyuwer@ya.ru>
2022-08-22 14:36:34 +0300

    homework task 27/29 added

---
+++ lesson 4 homework/dz 29/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите количество элементов массивa N: ");
+int element = int.Parse(Console.ReadLine());
+int[] mass=new int [element];
+for(int i=0; i<mass.Length; i++)
+    {
+    System.Console.WriteLine("Введите элементы массива N: ");
+    mass[i]=int.Parse(Console.ReadLine());
+    }
+Console.WriteLine("Массив N: ");
+for(int i=0; i<mass.Length; i++)
+    {
+    Console.WriteLine(mass[i]);
+    }
