8d839b9f52883a1f7805ea1c079836db8389cf11
abarkalova28 <tyuwer@ya.ru>
2022-08-22 14:08:04 +0300

    reworked homework task 25

---
+++ lesson 4 homework/dz 25/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write("Программа возводит число А в степень B \r\n");
+Console.WriteLine("Введите число A: ");
+int A=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число A: ");
+int B=Convert.ToInt32(Console.ReadLine());
+int result=A;
+for(int i=1; i<B; i++)
+{
+    result=result*A;
+}
+Console.Write("Число A в степени B равно ");
+Console.WriteLine(result);
