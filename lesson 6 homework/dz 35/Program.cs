6932d3464b7bdc547b08572591e85abedbde32b8
abarkalova28 <tyuwer@ya.ru>
2022-08-29 16:11:48 +0300

    test task 35, 37 added

---
+++ lesson 6 homework/dz 35/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+int [] array=new int[123];
+int count=0;
+for(int i = 0; i < array.Length; i++)
+    {
+    array[i] = new Random().Next(1,123);
+    Console.Write(array[i]+" ");
+    if (array[i]>10 && array[i]<99)
+        {
+         count=count+1;
+        }
+    }
+Console.WriteLine();
+Console.Write("Количество элементов в отрезке [10,99]: ");
+Console.WriteLine(count);
---
+++ lesson 6 homework/dz 37/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+Console.WriteLine("Массив:");
+int N = 10;
+int[] array = new int[N];
+
+for (int i = 0; i < array.Length; i++)
+    {
+    array[i] = new Random().Next(0, 10);
+    Console.Write(array[i] + " ");
+    }
+Console.WriteLine();
+Console.WriteLine("Произведение пар чисел в массиве:");
+int j = array.Length - 1;
+for (int i = 0; i < (array.Length + 1) / 2; i++)
+    {
+    Console.Write(array[i] * array[j] + " ");
+    j--;
+    }
